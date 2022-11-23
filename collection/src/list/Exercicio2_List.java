ed6e4f617b35c5e016f42c670f80102568ee0902
MarcosVinicius15 <marcosviniciusconrado@gmail.com>
2022-11-22 23:47:15 -0300

    Exercicio 2 Listas

--- collection/src/list/Exercicio2_List.java
+++ collection/src/list/Exercicio2_List.java
@@ -11,5 +11,52 @@ Se a pessoa responder positivamente a 2 questões ela deve ser classificada como
 "Cúmplice" e 5 como "Assassina". Caso contrário, ele será classificado como "Inocente".
 */
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercicio2_List {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<String> respostas = new ArrayList<>();
+
+        System.out.print("Telefonou para a vítima? [S/N] ");
+        respostas.add(scan.next());
+
+        System.out.print("\nEsteve no local do crime? [S/N] ");
+        respostas.add(scan.next());
+
+        System.out.print("\nMora perto da vítima? [S/N] ");
+        respostas.add(scan.next());
+
+        System.out.print("\nDevia para a vítima? [S/N] ");
+        respostas.add(scan.next());
+
+        System.out.print("\nJá trabalhou com a vítima? [S/N] ");
+        respostas.add(scan.next());
+
+        System.out.println(respostas);
+
+        Iterator<String> iterator = respostas.iterator();
+        int count = 0;
+        while (iterator.hasNext()){
+            if (iterator.next().equalsIgnoreCase("s")) count += 1;
+        }
+
+        switch (count){
+            case 2:
+                System.out.println("Suspeita");
+                break;
+            case 3:
+            case 4:
+                System.out.println("Cúmplice");
+                break;
+            case 5:
+                System.out.println("Assassina");
+                break;
+            default:
+                System.out.println("Inocente");
+        }
+    }
 }
